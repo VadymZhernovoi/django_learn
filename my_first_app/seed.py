7162f7b9f9028b5d0e70a3cca00d2c32b2114b0e
vadymzhernovoi <zhernovoivadym@gmail.com>
2025-09-05 17:05:53 +0200

    added seed

--- my_first_app/seed.py
+++ my_first_app/seed.py
@@ -12,14 +12,14 @@ def create_categories(count=100):
         Category.objects.create( name=faker.sentence(nb_words=5))
     print('Categories created.')
 
-# def create_tasks(count=100):
-#     for _ in range(count):
-#         Task.objects.create(
-#             title=faker.text(max_nb_chars=50),
-#             description=faker.text(max_nb_chars=550),
-#             status=random.choice(["New", "In Progress", "Pending", "Blocked", "Done"]),
-#             created_at=faker.date_time(),
-#             deadline=faker.date_time(),
-#         )
-#     print('Tasks created.')
+def create_tasks(count=100):
+    for _ in range(count):
+        Task.objects.create(
+            title=faker.text(max_nb_chars=50),
+            description=faker.text(max_nb_chars=550),
+            status=random.choice(["New", "In Progress", "Pending", "Blocked", "Done"]),
+            created_at=faker.date_time(),
+            deadline=faker.date_time(),
+        )
+    print('Tasks created.')
 
